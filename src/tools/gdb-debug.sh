5bde344d66029ee5bc7d9edb3b3ecd4387f3c54d
Sam Whitehead <sam.everythingcomputers@gmail.com>
2021-08-11 15:12:53 +0100

    Cleanup unused files

--- src/tools/gdb-debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# $1 is disk file
-# $2 is elf file with debug symbols
-# $3 is gdb script file
-
-@QEMU_BIN@ -s -S @QEMU_ARGS_2@ -kernel "@BINNAME@" &
-@GDB_BIN@ "@ELFNAME@" -command="@GDB_SCRIPTFILE@"
