e2c4f2462af7a16ae479affae81702e510e522e9
Sam Whitehead <sam.everythingcomputers@gmail.com>
2021-12-28 01:32:18 +0000

    Modifications to heap.c

--- src/kernel/heap.c
+++ src/kernel/heap.c
@@ -19,17 +19,18 @@
  * THE SOFTWARE.
  */
 
+/* This file's contents were originally copied from
+ * https://github.com/jncronin/rpi-boot/blob/master/heap.c
+ * The original file was GPL'd (2.0), as is this project.
+ */
+
+
 #include <stdint.h>
 #include <stddef.h>
 
 #define MAX_BRK (((uintptr_t)&_start - 0x8000) + 0xf0000)
 
-#ifdef ENABLE_USB
-#include "dwc_usb.h"
-#define MAX_BUF DWC_USB_FIFO_START
-#else
 #define MAX_BUF 0x100000
-#endif
 
 extern char _start, __end;
 
@@ -38,50 +39,48 @@ uintptr_t cur_buf = MAX_BRK;
 
 uintptr_t alloc_buf(size_t size)
 {
-	uintptr_t old_buf = cur_buf;
+    uintptr_t old_buf = cur_buf;
 
-	cur_buf += size;
-	if(cur_buf > MAX_BUF)
-	{
-		cur_buf = old_buf;
-		return 0;
-	}
-	// Align up to a 512 byte value
-	if(cur_buf & 0x1ff)
-	{
-		cur_buf &= ~0x1ff;
-		cur_buf += 0x200;
-	}
-	return old_buf;
+    cur_buf += size;
+    if(cur_buf > MAX_BUF) {
+        cur_buf = old_buf;
+        return 0;
+    }
+    // Align up to a 512 byte value
+    if(cur_buf & 0x1ff) {
+        cur_buf &= ~0x1ffl;
+        cur_buf += 0x200;
+    }
+    return old_buf;
 }
 
-void *sbrk(uint32_t increment)
+/* Increment the program break (or decrement for a negative value)
+ * The program break will always be aligned on a page (4096 byte)
+ * boundary, so if the value isn't a multiple of 0x1000 then the break will be
+ * moved forwards to the next page.
+ */
+void *sbrk(ptrdiff_t increment)
 {
-	// sbrk returns the previous brk value
+    // sbrk returns the previous brk value
 
-	// First set up cur_brk if not done already
-	if(cur_brk == 0)
-	{
-#ifdef DEBUG2
-		printf("HEAP: initializing at %p\n", &__end);
-#endif
+    // First set up cur_brk if not done already
+    if(cur_brk == 0) {
+        // printf("HEAP: initializing at %p\n", &__end);
+        cur_brk = (uintptr_t)&__end;
+        // If not aligned on a page boundary, move to the next page.
+        if(cur_brk & 0xfff) {
+            cur_brk &= ~0xfffl;
+            cur_brk += 0x1000;
+        }
+    }
 
-		cur_brk = (uintptr_t)&__end;
-		if(cur_brk & 0xfff)
-		{
-			cur_brk &= 0xfffff000;
-			cur_brk += 0x1000;
-		}
-	}
+    uintptr_t old_brk = cur_brk;
 
-	uintptr_t old_brk = cur_brk;
-
-	cur_brk += increment;
-	if(cur_brk >= MAX_BRK)
-	{
-		cur_brk = old_brk;
-		return (void*)-1;
-	}
-	return (void*)old_brk;
+    cur_brk += increment;
+    if(cur_brk >= MAX_BRK) {
+        cur_brk = old_brk;
+        return (void*)-1;
+    }
+    return (void*)old_brk;
 }
 
