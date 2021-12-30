3be7750fe1db9e6ccd17c273f601dc421c2d7cc4
Sam Whitehead <sam.everythingcomputers@gmail.com>
2021-12-30 20:59:25 +0000

    add test files

---
+++ include/kernel/test.h
@@ -0,0 +1,6 @@
+#ifndef KERN_TEST_H
+#define KERN_TEST_H 1
+
+unsigned int test_all();
+
+#endif /* kern_test_h */
---
+++ src/kernel/test/main.c
@@ -0,0 +1,6 @@
+/* kernel/test/main.c
+ * Copyright Sam Whitehead, 2021
+ * Last updated 2021-12-30
+ */
+
+#include "kernel/test.h"
